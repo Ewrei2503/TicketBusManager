12cf8211eafac4cd361e98774b3904e8d8bf6ab5
Ewrei2503 <egor.kulesh.02@mail.ru>
2024-10-09 01:04:51 +0300

    2.Added JsonParseException checker method

--- src/main/java/com/jfb/lecture5/Main.java
+++ src/main/java/com/jfb/lecture5/Main.java
@@ -2,24 +2,41 @@ package com.jfb.lecture5;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jfb.lecture5.exceptions.IllegalStartDateException;
+import com.jfb.lecture5.exceptions.IllegalTicketTypeException;
 import com.jfb.lecture5.model.BusTicket;
 
 import java.util.Scanner;
 
 public class Main {
+
+  public static int parseError = 0;
   public static void main(String[] args) throws JsonProcessingException {
     int x = 0;
 
     do {
       String input = getInput();
-      BusTicket busTicket = new ObjectMapper().readValue(input, BusTicket.class);
-
-      // TODO: ticket validation
-
-      System.out.println(busTicket.toString());
+      BusTicket busTicket;
+      try {
+        busTicket = new BusTicket(getBusTicket(input));
+        System.out.println(busTicket);
+      }catch (IllegalTicketTypeException | IllegalStartDateException e){
+        System.err.println(e.getMessage());
+      }
       x++;
+    } while (x < 17);
+  }
 
-    } while (x < 5);
+  private static BusTicket getBusTicket(String input) throws JsonProcessingException {
+    try {
+      return new ObjectMapper().readValue(input, BusTicket.class);
+    }
+    catch (JsonParseException e) {
+      System.err.println("Parsing error");
+      input = input.replace('“','\"');
+      parseError++;
+      return getBusTicket(input);
+    }
   }
 
   private static String getInput() {
