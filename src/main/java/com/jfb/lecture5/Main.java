17a82b64f4ce1f2234d65200a607c2f658d84f9c
Ewrei2503 <egor.kulesh.02@mail.ru>
2024-10-09 01:20:43 +0300

    4.Added valid counter and totalScore()

--- src/main/java/com/jfb/lecture5/Main.java
+++ src/main/java/com/jfb/lecture5/Main.java
@@ -9,9 +9,15 @@ import com.jfb.lecture5.model.BusTicket;
 
 import java.util.Scanner;
 
+import static com.jfb.lecture5.exceptions.IllegalPriceException.price;
+import static com.jfb.lecture5.exceptions.IllegalStartDateException.startDate;
+import static com.jfb.lecture5.exceptions.IllegalTicketTypeException.ticketType;
+
 public class Main {
 
   public static int parseError = 0;
+  public static int valid = 0;
+
   public static void main(String[] args) throws JsonProcessingException {
     int x = 0;
 
@@ -21,11 +27,13 @@ public class Main {
       try {
         busTicket = new BusTicket(getBusTicket(input));
         System.out.println(busTicket);
+        valid++;
       }catch (IllegalTicketTypeException | IllegalStartDateException e){
         System.err.println(e.getMessage());
       }
       x++;
     } while (x < 17);
+    totalScore();
   }
 
   private static BusTicket getBusTicket(String input) throws JsonProcessingException {
@@ -42,5 +50,17 @@ public class Main {
 
   private static String getInput() {
     return new Scanner(System.in).nextLine();
+  }
+
+  private static void totalScore(){
+    System.out.println(
+            "Total = " + (parseError+ticketType+startDate+price) +
+            "\nValid = " + valid +
+            "\nMost popular violation = " + max(parseError,ticketType,startDate,price)
+    );
+  }
+
+  private static int max(int a, int b, int c, int d){
+    return Math.max(a,Math.max(Math.max(b,c),d));
   }
 }
